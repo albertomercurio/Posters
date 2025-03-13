a0865fa5e0a27e4a420182cec84d1974f58199f9
Alberto Mercurio <alberto.mercurio96@gmail.com>
2025-03-13 15:29:09 +0100

    Final version

--- Phonon Pumping by Modulating the Ultrastrong Vacuum/script.jl
+++ Phonon Pumping by Modulating the Ultrastrong Vacuum/script.jl
@@ -65,7 +65,7 @@ grid_content_main = make_content_box!(left_column[1, 1], "The System")
 
 pdf_doc = PDFDocument(read("figures/virtual_photons_mirror.pdf"))
 LTeX(grid_content_main[1, 1], pdf_doc, scale=0.6, alignmode = Outside(0, 0, 60, 40), tellwidth=false)
-Label(grid_content_main[2, 1], L"A cavity of frequency $\omega_a$ and a qubit of frequency $\omega_\sigma$, are in ultrastrong coupling (USC). The cavity also interacts with a mirror, whose vibration frequency is $\omega_b$. The frequency of the qubit is adiabatically modulated, and the USC virtual photon population oscillates in time. This causes the mirror to oscillate. Collecting the emission of both the USC systems and of the vibrating mirror, only the latter will produce a signal.", fontsize=CONTENT_BODY_FONT_SIZE, word_wrap=true, justification=:left)
+Label(grid_content_main[2, 1], "A cavity is ultrastrongly coupled to a qubit. The cavity also interacts with a vibrating mirror. The frequency of the qubit is adiabatically modulated, and the USC virtual photon population oscillations cause the exchitation of the mirror.", fontsize=CONTENT_BODY_FONT_SIZE, word_wrap=true, justification=:left)
 
 # ----------- ### The Model ### ------------
 
@@ -73,11 +73,11 @@ grid_content_main = make_content_box!(left_column[2, 1], "The Model")
 
 hamiltonian = raw"""
 \begingroup
-\fontsize{42pt}{48pt}\selectfont
+\fontsize{40pt}{46pt}\selectfont
 \begin{align*}
-\hat{H}(t) =& \ \hat{H}_{\rm R} + \hat{H}_{\rm opt} + \hat{H}_{\rm M}(t) \\
-\hat{H}_{\rm R} =& \ \omega_a \hat{a}^\dagger \hat{a} +\omega_{\sigma} \hat{\sigma}_+ \hat{\sigma}_- + \lambda (\hat{a} + \hat{a}^\dagger )(\hat{\sigma}_-  + \hat{\sigma}_+ ), \\
-\hat{H}_{\rm opt} =& \ \omega_{b}\hat{b}^\dagger \hat{b} + \frac{g}{2} (\hat{a}+\hat{a}^\dagger)^2 (\hat{b}^\dagger + \hat{b}) \\
+\hat{H}(t) =& \ \hat{H}_{\rm R} + \hat{H}_{\rm opt} + \hat{H}_{\rm M}(t) \\[20pt]
+\hat{H}_{\rm R} =& \ \omega_a \hat{a}^\dagger \hat{a} +\omega_{\sigma} \hat{\sigma}_+ \hat{\sigma}_- + \lambda (\hat{a} + \hat{a}^\dagger )(\hat{\sigma}_-  + \hat{\sigma}_+ ), \\[20pt]
+\hat{H}_{\rm opt} =& \ \omega_{b}\hat{b}^\dagger \hat{b} + \frac{g}{2} (\hat{a}+\hat{a}^\dagger)^2 (\hat{b}^\dagger + \hat{b}) \\[20pt]
 \hat{H}_{\rm M}(t) =& \ \frac{1}{2} \Delta_\omega \left[ 1 + \cos ( \omega_d t ) \right] \hat{\sigma}_+ \hat{\sigma}_-  = \Omega_{\sigma}(t) \hat{\sigma}_+ \hat{\sigma}_-
 \end{align*}
 \endgroup
@@ -89,24 +89,24 @@ Label(grid_content_main[2, 1], "The system evolves according to the Lindblad mas
 
 lindblad = raw"""
 \begingroup
-\fontsize{42pt}{48pt}\selectfont
+\fontsize{40pt}{46pt}\selectfont
 \begin{align*}
 \dot{\hat{\rho}} = -i \comm{\hat{H} (t)}{\hat{\rho}} + (1 + n_{\rm th}) \gamma_b \mathcal{D} \left[ \hat{b} \right] \hat{\rho} + n_{\rm th} \gamma_b \mathcal{D} \left[ \hat{b}^\dagger \right] \hat{\rho} + \gamma_{\rm D} \mathcal{D} \left[ \hat{b}^\dagger \hat{b} \right] \hat{\rho}
 \end{align*}
 \endgroup
 """
 tex = TEXDocument(lindblad, true, preamble=latex_preamble)
-LTeX(grid_content_main[3, 1], tex, alignmode = Outside(0, 0, 20, 5), tellwidth=false)
+LTeX(grid_content_main[3, 1], tex, alignmode = Outside(0, 0, 20, 10), tellwidth=false)
 
 # ----------- ### Dispersive Approximation ### ------------
 
 grid_content_main = make_content_box!(left_column[3, 1], "Dispersive Approximation")
 
-Label(grid_content_main[1, 1], L"In the regime $g \ll \omega_d \simeq \omega_b \ll \omega_a \simeq \omega_{\sigma}$, the entanglement between the mechanical motion and the USC subsystem is negligible, and the state of the system can be factored as $|{\Psi(t)}\rangle \simeq | \psi (t)\rangle \otimes |{\phi_b(t)}\rangle$, where $| \psi (t) \rangle$ and $| \phi_b(t) \rangle$ are the USC and mirror states, respectively. The mechanical Hamiltonian becomes", fontsize=CONTENT_BODY_FONT_SIZE, word_wrap=true, justification=:left)
+Label(grid_content_main[1, 1], L"In the regime $g \ll \omega_d \simeq \omega_b \ll \omega_a \simeq \omega_{\sigma}$, the entanglement between the mechanical motion and the USC subsystem is negligible, and the state of the system can be factored as $|{\Psi(t)}\rangle \simeq | \psi (t)\rangle \otimes |{\phi_b(t)}\rangle$, where $| \psi (t) \rangle$ and $| \phi_b(t) \rangle$ are the USC and mirror states, respectively.", fontsize=CONTENT_BODY_FONT_SIZE, word_wrap=true, justification=:left)
 
 hamiltonian = raw"""
 \begingroup
-\fontsize{42pt}{48pt}\selectfont
+\fontsize{40pt}{46pt}\selectfont
 \begin{align*}
 \hat{H}_b(t) &= \bra{\psi_{0}(t)}
     \hat{H}_{\rm opt} \ket{\psi_{0}(t)}  = \omega_b \hat{b}^\dagger \hat{b} + \frac{g}{2} \mathcal{N}(t) \left( \hat{b} + \hat{b}^\dagger \right)~,
@@ -122,26 +122,75 @@ Label(grid_content_main[3, 1], L"with $\mathcal{N}(t) = \langle{{\psi}_{0}(t)}|{
 
 grid_content_main = make_content_box!(right_column[1, 1], "Results")
 
-plots_grid = GridLayout(grid_content_main[1, 1])
+plot1_grid = GridLayout(grid_content_main[1, 1])
+plot2_grid = GridLayout(grid_content_main[2, 1])
+
+plot1_label_grid = GridLayout(plot1_grid[1, 2])
+plot2_label_grid = GridLayout(plot2_grid[1, 1])
 
 pdf_doc = PDFDocument(read("figures/closed_dynamics_rabi.pdf"))
-LTeX(grid_content_main[1, 1], pdf_doc, scale=2, alignmode = Outside(0, 0, 60, 40), tellwidth=false)
+LTeX(plot1_grid[1, 1], pdf_doc, scale=1.6, tellwidth=true)
+
+equation = raw"""
+\begingroup
+\fontsize{32}{36pt}\selectfont
+\begin{equation*}
+\mathcal{N}(t) = \sum_{k=-\infty}^{+\infty} \mathcal{N}_k e^{i k \omega_d t}
+\end{equation*}
+\endgroup
+"""
+tex = TEXDocument(equation, true, preamble=latex_preamble)
+LTeX(plot1_label_grid[1, 1], tex, alignmode = Outside(0, 0, 20, 5), tellwidth=false)
+
+# --
 
 pdf_doc = PDFDocument(read("figures/spectrum_and_nth_rabi.pdf"))
-LTeX(grid_content_main[2, 1], pdf_doc, scale=2, alignmode = Outside(0, 0, 60, 40), tellwidth=false)
+LTeX(plot2_grid[1, 2], pdf_doc, scale=1.6, tellwidth=true)
+
+# Label(plot2_label_grid[1, 1], "Some text", fontsize=CONTENT_BODY_FONT_SIZE, word_wrap=true, justification=:left)
+
+equation = raw"""
+\begingroup
+\fontsize{32}{36pt}\selectfont
+\begin{equation*}
+\expval*{\hat{b}^\dagger \hat{b}}_{\rm ss} &=& \frac{\gamma + \gamma_{\rm D}}{\gamma} \left|\expval*{\hat{b}}_{\rm ss} \right| ^2 + n_{\rm th}
+\end{equation*}
+
+with
+
+\begin{equation*}
+\langle \hat{b} \rangle_\mathrm{ss} = (g \mathcal{N}_{\bar{k}})/[2 \Delta_{\bar{k}} + i (\gamma + \gamma_{D})]
+\end{equation*}
+\endgroup
+"""
+tex = TEXDocument(equation, true, preamble=latex_preamble)
+LTeX(plot2_label_grid[1, 1], tex, alignmode = Outside(0, 0, 20, 5), tellwidth=false)
 
 # ----------- ### Experimental Proposal ### ------------
 
 grid_content_main = make_content_box!(right_column[2, 1], "Experimental Proposal")
 
 pdf_doc = PDFDocument(read("figures/circuitv2.pdf"))
-LTeX(grid_content_main[1, 1], pdf_doc, scale=1, alignmode = Outside(0, 0, 60, 40), tellwidth=false)
+LTeX(grid_content_main[1, 1], pdf_doc, scale=1.7, tellwidth=false)
 
-Label(grid_content_main[2, 1], L"(a) Lumped element circuit of a resonator containing a mechanical-compliant capacitor, capacitively coupled (though $C_{g}$) to a frequency tunable transmon qubit. (b) Design of a 60\,$\mu m$ mechanical drum with 200\,$nm$ vacuum gap to the bottom electrode.", fontsize=CONTENT_BODY_FONT_SIZE, word_wrap=true, justification=:left)
+Label(grid_content_main[2, 1], L"(a) Lumped element circuit of a resonator containing a mechanical capacitor, capacitively coupled to a frequency tunable transmon qubit. (b) Design of a 60\,$\mu m$ mechanical drum with 200\,$nm$ vacuum gap to the bottom electrode.", fontsize=CONTENT_BODY_FONT_SIZE, word_wrap=true, justification=:left)
+
+rowgap!(grid_content_main, 20)
 
 # ----------- ### References ### ------------
 
 grid_content_main = make_content_box!(right_column[3, 1], "References")
+
+Label(grid_content_main[1, 1], "[1] Frisk Kockum, Anton, et al., Nat. Rev. Phys. 1.1 (2019): 19-40.", fontsize=CONTENT_BODY_FONT_SIZE, justification=:left, halign=:left)
+
+Label(grid_content_main[2, 1], "[2] Aspelmeyer, Markus, et al., Rev. Mod. Phys. 86.4 (2014): 1391-1452.", fontsize=CONTENT_BODY_FONT_SIZE, justification=:left, halign=:left)
+
+Label(grid_content_main[3, 1], "[3] Minganti, Fabrizio, et al., SciPost Phys. 17.1 (2024): 027.", fontsize=CONTENT_BODY_FONT_SIZE, justification=:left, halign=:left)
+
+pdf_doc = PDFDocument(read("figures/qr-code.pdf"))
+LTeX(grid_content_main[4, 1], pdf_doc, scale=0.18, tellwidth=false)
+
+Label(grid_content_main[5, 1], "Scan the QR code to access the full paper", fontsize=24)
 
 
 # # ----------- Spacing ------------
@@ -154,6 +203,6 @@ rowgap!(right_column, 80) # Gap between right column elements
 
 # # ----------- Save ------------
 
-# save("poster.pdf", fig, pt_per_unit=1)
+save("poster.pdf", fig, pt_per_unit=1)
 
 fig
